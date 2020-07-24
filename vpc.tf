c89942de1f4843483de12ea909938d638b65a361
madhavi0891 <66946576+madhavi0891@users.noreply.github.com>
2020-07-24 10:21:56 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-  region     = "us-east-1"
-  access_key = "AKIA6BVISBY5JWMDRPAP"
-  secret_key = "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"
+  region     = "ap-southeast-1"
+  access_key = "AKIAJP5XI3UAHB7XKEGQ"
+  secret_key = "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"
 
 }
 
@@ -12,7 +12,7 @@ module "vpc" {
   name = "my-vpc"
   cidr = "172.31.0.0/16"
 
-  azs             = ["us-east-2a", "us-east-2b", "us-east-2c"]
+  azs             = ["ap-southeast-2a", "ap-southeast-2b", "ap-southeast-2c"]
   private_subnets = ["172.31.1.0/24", "172.31.2.0/24", "172.31.3.0/24"]
   public_subnets  = ["172.31.101.0/24", "172.31.102.0/24", "172.31.103.0/24"]
 
