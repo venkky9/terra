46e6447fa339160f5cee7632bd4493947f9a6821
madhavi0891 <66946576+madhavi0891@users.noreply.github.com>
2020-07-24 14:47:15 +0530

    Create vars.tf

---
+++ vars.tf
@@ -0,0 +1,17 @@
+variable "AWS_REGION" {
+  default = "us-west-2"
+}
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "oregon"
+}
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "mykey.pub"
+}
+variable "AMIS" {
+  type = "map"
+  default = {
+    us-west-2 = "ami-0a243dbef00e96192"
+    us-west-2 = "ami-003634241a8fcdec0"
+    us-west-2 = "ami-0fb16d6843cf910d7"
+  }
+}
