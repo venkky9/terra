390735a0d45a5625bf85f2addf0be0f32374ebd6
madhavi0891 <66946576+madhavi0891@users.noreply.github.com>
2020-07-24 11:01:56 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region     = "ap-southeast-1"
+  region     = "us-west-2"
   access_key = "AKIAJP5XI3UAHB7XKEGQ"
   secret_key = "MEq9UxkckwyGO7ozv5IplAeVetspy7EnUWCBhhvV"
 
@@ -8,7 +8,7 @@ resource "aws_instance" "example" {
 
   ami                    = "ami-078648cce0d33c256"
   instance_type          = "t2.micro"
-  key_name               = "tokyo"
+  key_name               = "oregon"
   vpc_security_group_ids = ["main"]
 
   tags = {
