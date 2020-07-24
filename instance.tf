2be35f8e5de667955f50045bc63b85cc4551f251
madhavi0891 <66946576+madhavi0891@users.noreply.github.com>
2020-07-24 15:00:17 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -8,7 +8,5 @@ subnet_id = "${aws_subnet.main-public-1.id}"
 # the security group
 vpc_security_group_ids = ["${aws_security_group.example-instance.id}"]
 
-# the public SSH key
-key_name = "${aws_key_pair.mykeypair.key_name}"
 
 }
