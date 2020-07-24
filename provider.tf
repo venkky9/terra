41aff275c1103037d842d19a44e13eb306952f15
madhavi0891 <66946576+madhavi0891@users.noreply.github.com>
2020-07-24 15:40:59 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region     = "us-west-2"
-  access_key = "AKIAJP5XI3UAHB7XKEGQ"
-  secret_key = "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"
+  access_key = "AKIAJSL27BSL5PQIRRDA "
+  secret_key = "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"
 
 }
