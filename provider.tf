2aff83722eb4023fbf36f9b9e2e50723bc963e16
madhavi0891 <66946576+madhavi0891@users.noreply.github.com>
2020-07-24 11:34:47 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -4,14 +4,3 @@ provider "aws" {
   secret_key = "MEq9UxkckwyGO7ozv5IplAeVetspy7EnUWCBhhvV"
 
 }
-resource "aws_instance" "example" {
-
-  ami                    = "ami-078648cce0d33c256"
-  instance_type          = "t2.micro"
-  key_name               = "oregon"
-  vpc_security_group_ids = ["main"]
-
-  tags = {
-    name   = "terraform_instance"
-  }
-}
